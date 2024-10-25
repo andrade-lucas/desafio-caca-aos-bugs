0bf2148aeb99f6dc388eb0e5b01d44d2b551eeff
Lucas Andrade <lucasilva108@gmail.com>
2024-10-24 23:15:15 -0300

    test: string extensions

--- unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
+++ unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
@@ -4,6 +4,27 @@ namespace Balta.Domain.Test.SharedContext.Extensions;
 
 public class StringExtensionsTests
 {
-    [Fact]
-    public void ShouldGenerateBase64FromString() => Assert.Fail();
+    [Theory]
+    [InlineData("ShouldGenerateBase64FromString1")]
+    [InlineData("ShouldGenerateBase64FromString2")]
+    [InlineData("ShouldGenerateBase64FromString3")]
+    public void ShouldGenerateBase64FromString(string value)
+    {
+        #region Arrange & Act
+        var base64Str = value.ToBase64();
+        #endregion
+
+        #region Assert
+        Assert.True(IsBase64String(base64Str));
+        #endregion
+    }
+
+    private bool IsBase64String(string base64)
+    {
+        if (string.IsNullOrEmpty(base64))
+            return false;
+
+        Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
+        return Convert.TryFromBase64String(base64, buffer, out _);
+    }
 }
